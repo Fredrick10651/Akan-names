e5a86a7c6dd7efaba0a889915ccdc6eb62bb1497
PerisOduol618 <oduolpepe618@gmail.com>
2020-07-19 08:41:56 +0300

    Add form validation for gender

--- js/script.js
+++ js/script.js
@@ -12,6 +12,24 @@ var result = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
  var maleNames =["Kwasi", " Kwadwo", "Kwabena", "Kwaku", "  Yaw", " Kofi"," Kwame"];
  var femaleNames  = ["Akosua", "Adwoa", " Abenaa","Akua", "Yaa"," Afua",  "Ama"];
 
+// Form validation
+if (document.getElementById(gender).checked) {
+    var gender = "male";
+} 
+else{
+    var gender = "female",
+}
+if  (dd <= 0 || dd >31) {
+    alert("Please, input valid date");
+}
+else if (mm <= 0 || mm > 12) {
+    alert("please, input valid month")
+}
+else if (month == 2 && dd >29) {
+    alert("Invalid day of month")
+}
+else if 
+
 
 
 

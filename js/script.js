828d003fbecd7e52cc57729a53ac5236bc34f73c
PerisOduol618 <oduolpepe618@gmail.com>
2020-07-19 08:38:53 +0300

    Add male and female names

--- js/script.js
+++ js/script.js
@@ -7,3 +7,11 @@ var yy = parseInt(year.toString().slice(2, 4));
 var century = document.getElementById("century").value;
 var cc = Math.ceil(year  /100);
 
+var result = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+
+ var maleNames =["Kwasi", " Kwadwo", "Kwabena", "Kwaku", "  Yaw", " Kofi"," Kwame"];
+ var femaleNames  = ["Akosua", "Adwoa", " Abenaa","Akua", "Yaa"," Afua",  "Ama"];
+
+
+
+
